a3d499ff907c1a7cb0c33b6d3b6997006e05fa16
Nathan Hadley <nathanhadley.pigeon@gmail.com>
2024-10-10 13:50:28 +0100

    idk anymore hehehehehehehehehe

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const quotes = [
-  "you are good",
-  "you are not bad"
+const quotes = {
+  hope: [
+    "Hope is being able to see that there is light despite all of the darkness",
+    ""
+  ],
+
+  faith: [
+
+  ],
+
+  love: [
+
+  ],
+  
+  confidence: [
+
+  ],
+  
+  kindness: [
+
+  ]
+}
+
+const quoteCategories = [
+  "hope",
+  "faith",
+  "love",
+  "confidence",
+  "kindness"
 ]
+
+const quoteCategory = quoteCategories[Math.floor(Math.random() * quoteCategories.length)];
 
 document.getElementById("quote").innerText = quotes[Math.floor(Math.random() * quotes.length)];
